5111043c440bee1efb42be2230be5ae7970ce579
Eddie Padin <eddie@entertainermedia.net>
2020-09-01 16:14:18 -0500

    Updates

---
+++ precache-manifest.5ebf4f55d00684f8793f9386d6d9355a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ffceda27725e67a14759c2ee206f5d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2c27f6f6a9c3bd410506",
+    "url": "./static/js/2.049f2e87.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.049f2e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64fc93c2be7ff6f6584d",
+    "url": "./static/js/main.9b4b28d9.chunk.js"
+  },
+  {
+    "revision": "2c503ac00d803eb06ab5",
+    "url": "./static/js/runtime-main.eb366eae.js"
+  }
+]);
